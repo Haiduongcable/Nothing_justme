b56947e9928db55225e432ebc27c6985c279759f
Haiduongcable <haiduong.nguyen2712@gmail.com>
2023-11-30 22:14:44 +0700

    uploadnote

--- download_model.py
+++ download_model.py
@@ -4,4 +4,4 @@
 # model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-14B", device_map="auto", trust_remote_code=True, cache_dir="qween").eval()
 from transformers import AutoModelForCausalLM, AutoTokenizer
 cache_dir = "/hdd4/duongnh/project/ZaloMath2023/pretrained"
-model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-rw-1b", device_map="auto", trust_remote_code=True, cache_dir=cache_dir).eval()
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-7B", device_map="auto", trust_remote_code=True, cache_dir=cache_dir).eval()
